d3ccbd4b02254c17d753e84ae87d202b2a93f5f4
uditdas84 <uditdas84@gmail.com>
2023-07-20 02:35:37 +0530

    application tested in postman

---
+++ api/test.py
@@ -0,0 +1,19 @@
+from fastapi import File, UploadFile
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.post("/upload")
+def upload(file: UploadFile = File(...)):
+    try:
+        contents = file.file.read()
+        with open(file.filename, 'wb') as f:
+            f.write(contents)
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        file.file.close()
+
+    return {"message": f"Successfully uploaded {file.filename}"}
+
